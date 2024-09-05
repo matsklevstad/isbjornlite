c47149c9259853eb769d0f1cc253fe739fa07a04
Mats Klevstad <mats.klevstad@sikt.no>
2024-09-05 20:56:56 +0200

    remove code

--- isbjornlite/src/pages/index.tsx
+++ isbjornlite/src/pages/index.tsx
@@ -1,17 +1,3 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
   return (
     <div className="bg-blue-100">
