c4f9848d7f3ecf62ccf267fe0e0296c141b6596d
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-09-06 19:59:06 +0200

    three fiber

--- isbjornlite/src/components/threejs/Beer.tsx
+++ isbjornlite/src/components/threejs/Beer.tsx
@@ -1,42 +1,38 @@
-import { useEffect, useState } from 'react';
-import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { useScene } from '@/contexts/SceneContext';
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
 
-export default function Beer() {
-    const scene = useScene();
-    const [loadedGLTF, setLoadedGLTF] = useState<GLTF | null>(null);
-
-    useEffect(() => {
-        if (!scene) return;
-
-        const loader = new GLTFLoader();
-
-        loader.load('/3d/beer.glb', (gltf: GLTF) => {
-            setLoadedGLTF(gltf);
-            const model = gltf.scene;
-            model.scale.set(1, 1, 1);
-            model.position.set(0, 0, -10);
-
-            scene.add(model);
-        });
-
-        return () => {
-            if (loadedGLTF) {
-                scene.remove(loadedGLTF.scene);
-            }
-        };
-    }, [scene]);
-
-    useEffect(() => {
-        if (loadedGLTF && scene) {
-            const model = loadedGLTF.scene;
-            scene.add(model);
-
-            return () => {
-                scene.remove(model);
-            };
-        }
-    }, [loadedGLTF, scene]);
-
-    return null;
+export function BeerModel({ ...props }) {
+  const { nodes, materials } = useGLTF('/3d/beer.glb')
+  return (
+    <group {...props} dispose={null}>
+      <group position={[0, -5, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Canmodel_obj_1.geometry}
+          material={materials.lambert5SG}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Canmodel_obj_2.geometry}
+          material={materials.lambert6SG}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Canmodel_obj_3.geometry}
+          material={materials.lambert4SG}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Canmodel_obj_4.geometry}
+          material={materials['Material.001']}
+        />
+      </group>
+    </group>
+  )
 }
+
+useGLTF.preload('/3d/beer.glb')
--- isbjornlite/src/pages/index.tsx
+++ isbjornlite/src/pages/index.tsx
@@ -1,15 +1,17 @@
-import ThreeScene from "@/components/threejs/ThreeScene";
-import Cube from "@/components/threejs/Cube";
-import Beer from "@/components/threejs/Beer";
+import { Canvas } from "@react-three/fiber";
+import { BeerModel } from "@/components/threejs/Beer";
+import { OrbitControls } from "@react-three/drei";
 
 
 export default function Home() {
   return (
-    <div>
-      <ThreeScene>
-        {/* <Cube /> */}
-        <Beer />
-      </ThreeScene>
+    <div className="w-screen h-screen">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={1} color="white"/>
+        <directionalLight color="white" position={[0, 100, 50]} intensity={10} />
+        <BeerModel />
+      </Canvas>
     </div>
   );
 }
