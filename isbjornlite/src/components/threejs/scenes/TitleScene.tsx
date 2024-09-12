8861f1310b038fbd5660d7471521640b44322f9c
Mats Klevstad <mats.klevstad@sikt.no>
2024-09-12 19:31:07 +0200

    still trying .env stuff

--- isbjornlite/src/components/threejs/scenes/TitleScene.tsx
+++ isbjornlite/src/components/threejs/scenes/TitleScene.tsx
@@ -1,6 +1,6 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
-import {BeerSpawner} from "../BeerSpawner";
+import { BeerSpawner } from "../BeerSpawner";
 import Box from "../Box";
 import Ground from "../Ground";
 import Title from "../Title";
@@ -9,42 +9,49 @@ import { Physics } from "@react-three/cannon";
 //import ScrollDownBtn from "@/components/buttons/ScrollDownBtn";
 
 export default function TitleScene() {
-    return (
-        <Suspense fallback={null}>
-            <Canvas className="bg-black" frameloop="demand">
-                <Physics gravity={[0, -13, 0]}>
-                    <PerspectiveCamera makeDefault position={[0, -40, 50]} fov={25} near={1}/>
-                    <OrbitControls
-                    enablePan={false}
-                    enableRotate={false}
-                    enableZoom={false}
-                    />
-                    {/* {BoxSpawner(1000)} */}
-                    <BeerSpawner interval={1200}/>
-                    <Title />
-                    <Box position={[10, 2, 0]}/>
-                    {/* <directionalLight 
+  return (
+    <Suspense fallback={null}>
+      <div className="w-full h-screen">
+        <Canvas className="bg-black " frameloop="demand">
+          <Physics gravity={[0, -13, 0]}>
+            <PerspectiveCamera
+              makeDefault
+              position={[0, -40, 50]}
+              fov={25}
+              near={1}
+            />
+            <OrbitControls
+              enablePan={false}
+              enableRotate={false}
+              enableZoom={false}
+            />
+            {/* {BoxSpawner(1000)} */}
+            <BeerSpawner interval={1200} />
+            <Title />
+            <Box position={[10, 2, 0]} />
+            {/* <directionalLight 
                     position={[200, 100, 70]}
                     intensity={0.4}
                     color="#e4ebf0"
                     /> */}
-                    <pointLight 
-                    position={[0, 20, 60]}
-                    intensity={15000}
-                    color="#ffffff"
-                    distance={65}
-                    />
-                    <spotLight
-                    position={[0, 30, 70]}
-                    intensity={13000}
-                    color="#ffffff"
-                    distance={100}
-                    />
-                    <ambientLight intensity={1} color="#ffffff"/>
-                    <Ground />
-                </Physics>
-            </Canvas>
-            {/* <ScrollDownBtn /> */}
-        </Suspense>
-    );
+            <pointLight
+              position={[0, 20, 60]}
+              intensity={15000}
+              color="#ffffff"
+              distance={65}
+            />
+            <spotLight
+              position={[0, 30, 70]}
+              intensity={13000}
+              color="#ffffff"
+              distance={100}
+            />
+            <ambientLight intensity={1} color="#ffffff" />
+            <Ground />
+          </Physics>
+        </Canvas>
+      </div>
+      {/* <ScrollDownBtn /> */}
+    </Suspense>
+  );
 }
--- isbjornlite/src/firebase.js
+++ isbjornlite/src/firebase.js
@@ -1,32 +1,31 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-/* LOCALHOST */
-/*const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};*/
-
-/* VERCEL */
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+const isLocal = process.env.NODE_ENV === "development";
+console.log("isLocal", isLocal);
+const firebaseConfig = isLocal
+  ? {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    }
+  : {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(firebaseConfig.apiKey);
 
 // Initialize analytics (optional)
 let analytics;
