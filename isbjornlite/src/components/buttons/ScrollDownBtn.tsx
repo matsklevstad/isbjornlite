008ef1022f05259827e63eb4fe272ee56b1d2c22
Mats Klevstad <mats.klevstad@sikt.no>
2024-09-12 18:32:00 +0200

    test new .env names

--- isbjornlite/src/components/buttons/ScrollDownBtn.tsx
+++ isbjornlite/src/components/buttons/ScrollDownBtn.tsx
@@ -1,23 +1,23 @@
-import {IconCircleArrowDownFilled} from '@tabler/icons-react';
+//import { IconCircleArrowDownFilled } from "@tabler/icons-react";
 
 const ScrollDownBtn = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("scrolltarget");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    } else {
+      console.error("No next section found");
+    }
+  };
 
-    const handleScroll = () => {
-        const nextSection = document.getElementById('scrolltarget');
-        if (nextSection) {
-            nextSection.scrollIntoView({ behavior: 'smooth' });
-        } else {
-            console.error('No next section found');
-        }
-    };
-
-    return (
-        <div className="flex w-full h-full absolute justify-center items-end p-10 z-10">
-            <button onClick={handleScroll}>
-                <IconCircleArrowDownFilled size={35} color="#ffffff"/>
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex w-full h-full absolute justify-center items-end p-10 z-10">
+      <button onClick={handleScroll}>
+        {/* <IconCircleArrowDownFilled size={35} color="#ffffff" /> */}
+       
+      </button>
+    </div>
+  );
 };
 
 export default ScrollDownBtn;
