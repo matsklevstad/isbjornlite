26eb67b8444b12cfb71bb3b4510e07b4401f28c9
Mats Klevstad <mats.klevstad@sikt.no>
2024-09-13 09:51:41 +0200

    add better support for mobile device

--- isbjornlite/src/pages/Log.tsx
+++ isbjornlite/src/pages/Log.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React, { useState, useEffect, useRef } from "react";
 import { Modal } from "@sikt/sds-modal";
 import "@sikt/sds-modal/dist/index.css";
 import { app } from "../firebase";
@@ -10,6 +8,9 @@ import {
   addDoc,
   onSnapshot,
 } from "firebase/firestore";
+import Image from "next/image";
+import { format } from "date-fns";
+import { nb } from "date-fns/locale";
 
 interface BeerLog {
   name: string;
@@ -20,11 +21,20 @@ export default function Log() {
   const [showModal, setShowModal] = useState(false);
   const [values, setValues] = useState<BeerLog[]>([]);
   const [name, setName] = useState("");
+  const logContainerRef = useRef<HTMLDivElement>(null); // Reference to log container
 
   const db = getFirestore(app);
 
-  // Function to add data to Firestore
-  async function addData() {
+  async function saveIsbjorn() {
+    if (!name || name.trim() === "") {
+      alert("Fyll ut navn!");
+      return;
+    }
+
+    if (name.length > 20) {
+      alert("Navnet kan ikke være lengre enn 20 tegn!");
+      return;
+    }
     try {
       const docRef = await addDoc(collection(db, "beers"), {
         name: name
@@ -33,49 +43,42 @@ export default function Log() {
           .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
           .join(" ")
           .trim(),
-        timestamp: new Date().toLocaleString(),
+        timestamp: new Date().toISOString(), // Store timestamp in ISO format
       });
       console.log("Document written with ID: ", docRef.id);
+      setName("");
+      setShowModal(false);
+      const audio = new Audio("/assets/open-beer-sound.mp3");
+      audio.play();
+      if (logContainerRef.current) {
+        logContainerRef.current.scrollTo({ top: 0, behavior: "smooth" });
+      }
     } catch (e) {
-      console.error("Error adding document: ", e);
+      alert("Error adding isbjørn :(");
     }
   }
 
-  // Listening for Firestore updates and setting values to state
   useEffect(() => {
-    AOS.init({ duration: 2000 });
-
     const unsubscribe = onSnapshot(collection(db, "beers"), (snapshot) => {
       const newValues: BeerLog[] = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...(doc.data() as BeerLog),
       }));
+
       newValues.sort(
         (a, b) =>
           new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
       );
+
       setValues(newValues);
-      const audio = new Audio("/assets/open-beer-sound.mp3");
-      audio.play();
     });
 
     return () => unsubscribe();
   }, [db]);
 
-  const saveIsbjorn = () => {
-    if (!name) {
-      alert("Please fill out all fields");
-      return;
-    }
-    addData();
-
-    setName("");
-    setShowModal(false);
-  };
-
   return (
     <div>
-      {/* Modal Component */}
+      {/* MODAL */}
       <Modal
         open={showModal}
         onClose={() => setShowModal(false)}
@@ -89,55 +92,64 @@ export default function Log() {
           <br />
           <br />
         </p>
-        <input
-          type="text"
-          placeholder="Navn"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="px-4 py-2  mr-4 border rounded w-full md:w-auto"
-        />
-        <button
-          onClick={saveIsbjorn}
-          className="px-4 py-2 bg-blue-500 text-white rounded w-full md:w-auto"
-        >
-          Legg til
-        </button>
+        <div className="flex flex-col md:flex-row gap-4 ">
+          <input
+            type="text"
+            placeholder="Navn"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="px-4 py-2 border rounded w-full md:w-1/2"
+          />
+          <button
+            onClick={saveIsbjorn}
+            className="px-4 py-2 bg-blue-500 text-white rounded w-full md:w-1/4"
+          >
+            Legg til
+          </button>
+        </div>
       </Modal>
 
-      {/* Main Content */}
-      <div
-        data-aos="fade-in"
-        data-aos-duration="600"
-        className="relative w-screen h-screen p-4"
-      >
-        <div className="flex flex-col gap-4 px-4 md:px-72 justify-center absolute top-0 bottom-0 left-0 right-0 overflow-y-auto pt-16">
-          {values.map((value, index) => (
-            <div
-              className="flex flex-col md:flex-row items-center p-4 bg-gray-800 border border-gray-700 rounded-lg shadow-lg"
-              key={index}
-            >
-              <img
-                src="https://www.mack.no/img/_productMedium/isbjorn-lite-fisheye-label.png"
-                alt="Isbjorn"
-                className="w-16 h-16 md:w-20 md:h-20 rounded-full border border-gray-600"
-              />
-              <div className="ml-4 text-white text-center md:text-left">
-                <p className="font-semibold text-blue-400 text-2xl">
-                  {value.name}
-                </p>
-                <p className="text-white  text-xl">{value.timestamp}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {/* Button to Open Modal */}
+      {/* MAIN CONTENT */}
+      <div className="w-screen h-screen bg-blue-400 p-4">
         <button
-          className="absolute z-20 top-4 right-4 px-4 py-2 bg-blue-500 text-white rounded"
+          className="z-20 top-4 right-4 px-4 py-2 bg-white text-black rounded"
           onClick={() => setShowModal(!showModal)}
         >
           Registrer ny isbjørn
         </button>
+
+        {/* Container with scrollable log */}
+        <div
+          ref={logContainerRef}
+          className="flex flex-col gap-4 md:gap-8 md:w-[60vw] px-4 md:px-72  overflow-y-auto h-[80vh] pt-16 mt-4"
+        >
+          {values.map((value, index) => (
+            <div
+              className="flex flex-row items-center p-4 bg-gray-800 border border-gray-700 rounded-lg shadow-lg"
+              key={index}
+            >
+              <div>
+                <Image
+                  src="/assets/isbjorn-lite-logo.png"
+                  alt="Isbjorn"
+                  width={80}
+                  height={80}
+                  className="rounded-full border border-gray-600"
+                />
+              </div>
+              <div className="ml-4 text-white text-center md:text-left">
+                <p className="font-semibold text-blue-400 text-2xl">
+                  {value.name}
+                </p>
+                <p className="text-white text-xl">
+                  {format(new Date(value.timestamp), "EEEE, dd. MMMM HH:mm", {
+                    locale: nb,
+                  })}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- isbjornlite/src/pages/_document.tsx
+++ isbjornlite/src/pages/_document.tsx
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {" "}
         <link
           rel="stylesheet"
           href="https://unpkg.com/aos@2.3.1/dist/aos.css"
