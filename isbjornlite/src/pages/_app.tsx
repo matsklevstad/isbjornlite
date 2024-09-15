024fb6fadc08932a3b1ebb778e96239562e61212
Mats Klevstad <matskl@stud.ntnu.no>
2024-09-15 14:01:06 +0200

    update meta data

--- isbjornlite/src/pages/_app.tsx
+++ isbjornlite/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Isbjørn Lites venner | 🍺🐻‍❄️</title>
+        <meta
+          name="description"
+          content="Et samfunn for alle som elsker Isbjørn Lite i ulike fasonger"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
