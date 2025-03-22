591b696a18b8456b7d4cac3bf3d63b70229c27bf
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-03-22 22:03:33 +0100

    update React and ReactDOM to version 18; remove unused beer components and routes; adjust TypeScript configuration for better clarity

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import TitleScene from "@/components/threejs/scenes/TitleScene";
 
-import IsbjornLive from "../components/threejs/scenes/IsbjornLive";
-
 export default function Index() {
   return (
     <div className="overflow-x-hidden">
       <TitleScene />
-      <IsbjornLive />
     </div>
   );
 }
