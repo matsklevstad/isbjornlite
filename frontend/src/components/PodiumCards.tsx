b66bb9a5c6e4350a6cd254b52c0b640ba8d62b93
Mats Klevstad <matsi99@live.com>
2025-03-29 14:08:03 +0100

    feat: enhance PodiumCards layout and improve index page structure

--- frontend/src/components/PodiumCards.tsx
+++ frontend/src/components/PodiumCards.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Text, Center, Stack, Group } from "@mantine/core";
+import { Card, Text, Center, Stack, Group, Badge } from "@mantine/core";
 import { useRouter } from "next/router";
 
 interface Props {
@@ -40,14 +40,13 @@ const PodiumCards = (props: Props) => {
               position: "relative",
               top: cardConfig[index].top,
             }}
-            onClick={() => handleUserClick(user.userId)}>
-            <Stack align="center" justify="space-between" h="100%">
-              <Text fz={{ base: "1rem", sm: "2rem" }} fw={700}>
-                {user.username}
-              </Text>
-              <Text fz={{ base: "2rem", sm: "4rem" }} fw={800}>
-                {user.totalBeers}
-              </Text>
+            onClick={() => handleUserClick(user.userId)}
+          >
+            <Stack align="center" justify="space-around" h="100%">
+              <Text fz={{ base: "1rem", sm: "2rem" }}>{user.username}</Text>
+                <Text fz={{ base: "2rem", sm: "4rem" }} fw={600}>
+                  {user.totalBeers}
+                </Text>
             </Stack>
           </Card>
         ))}
