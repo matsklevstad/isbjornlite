7133726a1125bc072efe707d36cd088e7f3368a9
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-03-29 11:47:22 +0100

    feat: add animations to login button and title for improved user experience

--- frontend/src/components/homepage/buttons/LogInBtn.tsx
+++ frontend/src/components/homepage/buttons/LogInBtn.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { Title } from "@mantine/core";
 import { IconLogin2 } from "@tabler/icons-react";
 import styles from "./styles/LogInBtn.module.css";
+import { motion } from "framer-motion";
 
 export default function LogInBtn() {
   const router = useRouter();
@@ -12,13 +13,26 @@ export default function LogInBtn() {
 
   return (
     <div className={styles.container}>
-      <Title size={26} c={"white"}>Har du drukket isbjørn?</Title>
-      <button onClick={handleLoginClick} className={styles.button}>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className={styles.title}>
+        <Title size={26} c={"white"}>
+          Har du drukket isbjørn?
+        </Title>
+      </motion.div>
+      <motion.button
+        initial={{ scale: 1, opacity: 0, x: -50 }}
+        whileInView={{ scale: 1, opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, type: "spring", bounce: 0.4, delay: 0.2 }}
+        onClick={handleLoginClick}
+        className={styles.button}>
         <span>
           Logg Inn
           <IconLogin2 className={styles.icon} />
         </span>
-      </button>
+      </motion.button>
     </div>
   );
 }
